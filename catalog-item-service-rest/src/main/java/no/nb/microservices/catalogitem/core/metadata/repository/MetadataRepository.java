061f5a82b5a4b6faa7916c6aa6de3d7c9a9d5928
ronnymikalsen <ronny.mikalsen@nb.no>
2015-06-08 15:16:40 +0200

    feign

--- catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/core/metadata/repository/MetadataRepository.java
+++ catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/core/metadata/repository/MetadataRepository.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "catalog-metdata-service")
+@FeignClient("catalog-metdata-service")
 public interface MetadataRepository {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}/mods")
