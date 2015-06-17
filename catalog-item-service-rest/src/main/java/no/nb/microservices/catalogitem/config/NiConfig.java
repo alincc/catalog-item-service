29a06ca7b3867a1b9ff57475266def6776312c8c
ronnymikalsen <ronny.mikalsen@nb.no>
2015-06-17 12:40:50 +0200

    Docker profile

--- catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/config/NiConfig.java
+++ catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/config/NiConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Profile;
  *
  */
 @Configuration
-@Profile("prod")
+@Profile("docker")
 public class NiConfig {
 
     private final NiSettings niSetting;
