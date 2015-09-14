92a0117a91dce9bb82c2b14d84ad5e6cd387480a
ronnymikalsen <ronny.mikalsen@nb.no>
2015-09-14 10:56:33 +0200

    Removed unused imports

--- catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/rest/controller/assembler/GeographicBuilder.java
+++ catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/rest/controller/assembler/GeographicBuilder.java
@@ -2,11 +2,7 @@ package no.nb.microservices.catalogitem.rest.controller.assembler;
 
 import no.nb.microservices.catalogitem.rest.model.Geographic;
 import no.nb.microservices.catalogmetadata.model.mods.v3.OriginInfo;
-import no.nb.microservices.catalogmetadata.model.mods.v3.Place;
 
-/**
- * Created by andreasb on 11.09.15.
- */
 public class GeographicBuilder {
 
     private OriginInfo originInfo;
