84eeae25492ff8684d24cabede290f240007749c
ronnymikalsen <ronny.mikalsen@nb.no>
2015-09-16 12:25:31 +0200

    Added ignore

--- catalog-item-service-model/src/main/java/no/nb/microservices/catalogitem/rest/model/Metadata.java
+++ catalog-item-service-model/src/main/java/no/nb/microservices/catalogitem/rest/model/Metadata.java
@@ -1,10 +1,12 @@
 package no.nb.microservices.catalogitem.rest.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Metadata {
     private String compositeTitle;
     private TitleInfo titleInfo;
