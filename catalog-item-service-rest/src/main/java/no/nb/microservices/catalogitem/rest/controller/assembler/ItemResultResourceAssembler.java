5ebbf61dfe9007e71a7b223dcab9f3378d9a2f66
ronnymikalsen <ronny.mikalsen@nb.no>
2015-11-03 13:31:49 +0100

    sonar

--- catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/rest/controller/assembler/ItemResultResourceAssembler.java
+++ catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/rest/controller/assembler/ItemResultResourceAssembler.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 
 import no.nb.microservices.catalogitem.core.item.model.Item;
