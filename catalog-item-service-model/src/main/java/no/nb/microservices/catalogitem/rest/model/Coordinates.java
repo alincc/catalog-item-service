51d0790e978e3445f738bf2ad40852e0385e9c7a
Jim Arthur Nilsen <jim.nilsen@nb.no>
2015-12-02 10:08:11 +0100

    Added empty constructor

--- catalog-item-service-model/src/main/java/no/nb/microservices/catalogitem/rest/model/Coordinates.java
+++ catalog-item-service-model/src/main/java/no/nb/microservices/catalogitem/rest/model/Coordinates.java
@@ -9,6 +9,10 @@ public class Coordinates {
     private double latitude;
     private double longitude;
 
+    public Coordinates() {
+
+    }
+
     public Coordinates(double latitude, double longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
