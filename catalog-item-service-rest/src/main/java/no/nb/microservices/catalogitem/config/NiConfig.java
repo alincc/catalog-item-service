60acea7136a27ba9ebd4fc8984b2ceb7461820ab
Andreas Bjørnådal <bjornadal@gmail.com>
2016-03-10 10:26:13 +0100

    Removed local profile, Fixed bootstrap -- REVERT

--- catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/config/NiConfig.java
+++ catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/config/NiConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile({"docker", "dev"})
+@Profile({"docker", "dev", "local"})
 public class NiConfig {
 
     @Autowired
