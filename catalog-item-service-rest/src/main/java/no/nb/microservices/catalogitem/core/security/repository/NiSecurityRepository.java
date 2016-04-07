06f567ae121ca19f8ef1d04bedd804cef2675e78
alfredkw <alfred.wahle@nb.no>
2016-04-07 08:52:56 +0200

    - Hystrix fallback på ni

--- catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/core/security/repository/NiSecurityRepository.java
+++ catalog-item-service-rest/src/main/java/no/nb/microservices/catalogitem/core/security/repository/NiSecurityRepository.java
@@ -1,10 +1,10 @@
 package no.nb.microservices.catalogitem.core.security.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import no.nb.htrace.annotation.Traceable;
 import no.nb.sesam.ni.niclient.NiClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class NiSecurityRepository implements SecurityRepository {
@@ -19,12 +19,17 @@ public class NiSecurityRepository implements SecurityRepository {
 
     @Override
     @Traceable(description="NI hasAccess")
+    @HystrixCommand(fallbackMethod = "hasAccessFallback")
     public boolean hasAccess(String id, String clientIp, String ssoToken) {
         try {
             return niClient.hasAccess(ssoToken, id, null, clientIp);
         } catch (Exception ex) {
             throw new SecurityException("Error getting access info for id " + id, ex);
         }
+    }
+
+    private boolean hasAccessFallback(String id, String clientIp, String ssoToken) {
+        return false;
     }
 
 }
